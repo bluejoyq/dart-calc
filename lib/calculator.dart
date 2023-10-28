68cecde995c58cff128cb458d86c6cdee3de4157
bluejoyq <bluejoy32@gmail.com>
2023-10-28 17:42:58 +0900

    feat: calculator 뺄셈 기능 추가

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -2,4 +2,8 @@ class Calculator {
   num addition(num a, num b) {
     return a + b;
   }
+
+  num subtraction(num a, num b) {
+    return a - b;
+  }
 }
--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -4,11 +4,29 @@ import 'package:test/test.dart';
 void main() {
   Calculator calculator = new Calculator();
   group('덧셈', () {
-    test('정수와 정수를 더한다', () {
+    test('두 정수의 합', () {
       expect(calculator.addition(1, 2), 3);
     });
-    test('정수와 실수를 더한다', () {
+    test('정수와 실수의 합', () {
       closeTo(calculator.addition(1, -1.1), -0.1);
+    });
+
+    test('두 실수의 합이 정수가 되는 경우', () {
+      expect(calculator.addition(1.1, -1.1), 0);
+    });
+  });
+
+  group('뺄셈', () {
+    test('두 정수의 차', () {
+      expect(calculator.subtraction(1, 2), -1);
+    });
+
+    test('정수와 실수의 차', () {
+      closeTo(calculator.subtraction(1, -1.1), 2.1);
+    });
+
+    test('두 실수의 차가 정수가 되는 경우', () {
+      expect(calculator.subtraction(3.1, 1.1), 2);
     });
   });
 }
