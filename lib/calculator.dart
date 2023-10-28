aa6a384d90fa34f0d8992a45cf79ffd60beb7132
bluejoyq <bluejoy32@gmail.com>
2023-10-29 02:03:11 +0900

    feat: calculator의 2번째 피연산자를 nullable로

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -2,27 +2,32 @@ import 'package:dart_calc/models/app_number.dart';
 
 class Calculator {
   late AppBaseNumber operand1;
-  late AppBaseNumber operand2;
-  late String operator;
+  AppBaseNumber? operand2;
+  String operator = '';
   Calculator(List<String> args) {
     if (args.length > 3) {
       throw RangeError('인수가 많습니다.');
     }
+
     operand1 = AppNumber(num.parse(args[0]));
     operator = args[1];
-    operand2 = AppNumber(num.parse(args[2]));
+    try {
+      operand2 = AppNumber(num.parse(args[2]));
+    } catch (_) {
+      operand2 = null;
+    }
   }
 
   AppBaseNumber operate() {
     switch (operator) {
       case '+':
-        return operand1 + operand2;
+        return operand1 + operand2!;
       case '-':
-        return operand1 - operand2;
+        return operand1 - operand2!;
       case '*':
-        return operand1 * operand2;
+        return operand1 * operand2!;
       case '/':
-        return operand1 / operand2;
+        return operand1 / operand2!;
       default:
         throw UnsupportedError('지원하지 않는 연산자입니다.');
     }
