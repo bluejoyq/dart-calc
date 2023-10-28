49176a43795782fd69bef2c8e7c4ab99d7253ef2
bluejoyq <bluejoy32@gmail.com>
2023-10-29 03:31:56 +0900

    refactor: main error 핸들링 단순화

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,6 @@ void main(List<String> args) {
     final calculator = Calculator(args);
     final result = calculator.operate();
     print(result);
-  } on RangeError catch (e) {
-    print(e.message);
-  } on UnsupportedError catch (e) {
-    print(e.message);
-  } on ZeroDivisionException catch (e) {
-    print(e.message);
   } catch (e) {
     print(e);
   }
